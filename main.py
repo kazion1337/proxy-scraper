243c7f7c0bceb50e1d48ce5158734d4d4396dfd7
kazion1337 <70340957+kazion1337@users.noreply.github.com>
2021-02-02 13:40:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+def scrape():
+    scraped = 0
+    f = open("proxies.txt", "a+")
+    f.truncate(0)
+    r = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=https&timeout=1500&ssl=yes')
+    proxies = []
+    for proxy in r.text.split('\n'):
+        proxy = proxy.strip()
+        if proxy:
+            proxies.append(proxy)
+    for p in proxies:
+        scraped = scraped + 1 
+        f.write((p)+"\n")
+    f.close()
+
+scrape()
